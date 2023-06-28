3c3c693908d9f17da69d24509ca7dd1bf5fd8f6f
Arturo Mora-Rioja <arturomora@gmail.com>
2023-06-28 10:08:36 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/**
+ * Web performance example: lazy loading of images
+ * 
+ * @author  Arturo Mora-Rioja (amri@kea.dk)
+ * @version 1.0.0, December 2022
+ */
+ 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM content fully loaded');
+    console.log('First image loading status: ' + document.querySelector('img:first-of-type').complete);
+    console.log('Last image loading status: ' + document.querySelector('img:last-of-type').complete);
+
+    setTimeout(() => {
+        console.log('Wait for half a second...');
+        console.log('First image loading status: ' + document.querySelector('img:first-of-type').complete);
+        console.log('Last image loading status: ' + document.querySelector('img:last-of-type').complete);    
+    }, '500');
+});
