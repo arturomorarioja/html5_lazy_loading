84a7907415ee195e2327e39b0f2db15c01b8bd91
Arturo Mora-Rioja <arturomora@gmail.com>
2024-01-23 12:08:53 +0100

    Refactoring

--- js/script.js
+++ js/script.js
@@ -3,17 +3,17 @@
  * 
  * @author  Arturo Mora-Rioja (amri@kea.dk)
  * @version 1.0.0, December 2022
+ * @version 1.0.1, January 2024. Refactoring
  */
  'use strict';
 
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('DOM content fully loaded');
-    console.log('First image loading status: ' + document.querySelector('img:first-of-type').complete);
-    console.log('Last image loading status: ' + document.querySelector('img:last-of-type').complete);
+console.log('DOM content fully loaded');
 
-    setTimeout(() => {
-        console.log('Wait for half a second...');
-        console.log('First image loading status: ' + document.querySelector('img:first-of-type').complete);
-        console.log('Last image loading status: ' + document.querySelector('img:last-of-type').complete);    
-    }, '500');
-});
+console.log('First image loading status: ' + document.querySelector('img:first-of-type').complete);
+console.log('Last image loading status: ' + document.querySelector('img:last-of-type').complete);
+
+setTimeout(() => {
+    console.log('Wait for half a second...');
+    console.log('First image loading status: ' + document.querySelector('img:first-of-type').complete);
+    console.log('Last image loading status: ' + document.querySelector('img:last-of-type').complete);    
+}, '500');
